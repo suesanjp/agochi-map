5c70ab0b346ded088f952d6961fe9761bc3dac42
suesanjp <a.drop.of.my.essence@gmail.com>
2021-09-10 20:04:53 +0900

    add/scraping

--- scraiping/main.py
+++ scraiping/main.py
@@ -5,24 +5,38 @@ import csv
 import time
 
 # csvヘッダー
-HEADER = ["num", "name", "address", "area", "genre", "coordinates"]
+HEADER = [
+    "num",
+    "name",
+    "area",
+    "genre",
+    "address",
+    "tel",
+    "business_hours",
+    "holiday",
+    "coordinates",
+]
 num = 1
 
 # csvに保存
 with open("agochi.csv", "w", encoding="utf-8") as f:
     writer = csv.writer(f)
     writer.writerow(HEADER)
-    for i in range(1):
+    for i in range(10):
         url = "https://agochi.jp/introduce/a" + str(num) + "/"
         page = requests.get(url)
         soup = BeautifulSoup(page.text, "html.parser")
 
         # 店舗情報を取得
         name = soup.h2.string
-        soup_table = soup.table
-        address = soup.table.select("p:nth-child(1)")
         area = soup.select("a.cat-area")[0]["href"]
         genre = soup.select("a.cat-genre")[0]["href"]
+
+        soup_table = soup.table
+        address = soup_table.select_one("tr:nth-child(1) > td > p ").text
+        tel = soup_table.select_one("tr:nth-child(2) > td > p").text
+        business_hours = soup_table.select_one("tr:nth-child(3) > td > p").text
+        holiday = soup_table.select_one("tr:nth-child(4) > td > p").text
 
         # 埋め込みGoogleマップから緯度経度を取得
         ifarme_src = soup.find("iframe").get("src")
@@ -31,6 +45,17 @@ with open("agochi.csv", "w", encoding="utf-8") as f:
             r"\d{2}\.\d{4,20}\,\d{3}\.\d{4,20}", iframe_page
         ).group()
 
-        row = [num, name, address, area, genre, coordinates]
+        row = [
+            num,
+            name,
+            area,
+            genre,
+            address,
+            tel,
+            business_hours,
+            holiday,
+            coordinates,
+        ]
         writer.writerow(row)
         num += 1
+        time.sleep(0.5)
