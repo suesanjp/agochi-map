1ba6c6c2cba6e874e9001a468743484d1dd7c5de
suesanjp <a.drop.of.my.essence@gmail.com>
2021-09-12 22:04:28 +0900

    add/Detect Locatin

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Map from "./components/Map";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+
+      {/* これがないと地図がばらばらになる*/}
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+      />
+
+      <Map />
+    </div>
+  );
+}
+
+export default App;
