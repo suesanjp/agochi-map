ea2a1885feb6e3d699ce96c01ad5d3cbec5d9bcf
suesanjp <a.drop.of.my.essence@gmail.com>
2021-09-15 15:09:40 +0900

    add/ export to Json file

--- scraiping/main.py
+++ scraiping/main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
+import json
 
 num = 1
 dict = {}
@@ -39,6 +40,12 @@ for i in range(2):
     dict["coordinates" + str(num)] = coordinates
     print(dict)
 
+    # 辞書オブジェクトをstr型で取得して出力
+    print(json.dumps(dict, ensure_ascii=False, indent=2))
+
+    # 辞書オブジェクトをJSONファイルへ出力
+    with open("mydata.json", mode="wt", encoding="utf-8") as file:
+        json.dump(dict, file, ensure_ascii=False, indent=2)
 
     num += 1
     time.sleep(0.5)
