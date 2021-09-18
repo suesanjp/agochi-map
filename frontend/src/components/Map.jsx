12e281beba58f742e8655fa762e75895fd645fb6
suesanjp <a.drop.of.my.essence@gmail.com>
2021-09-18 11:16:29 +0900

    fix/ separate Marker and Popup file

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,9 +1,7 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
+import MarkerPopup from "./MarkerPopup";
 import LocationMarker from "./Location";
-import mydata from "../mydata.json";
 
-const num = 0;
-const name = mydata[num].name;
 // TODO intで緯度、経度それぞれjsonに出力するように
 
 const Map = () => {
@@ -17,9 +15,7 @@ const Map = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[43.7332904, 142.3887079]}>
-        <Popup>{name}</Popup>
-      </Marker>
+      <MarkerPopup />
       <LocationMarker />
     </MapContainer>
   );
