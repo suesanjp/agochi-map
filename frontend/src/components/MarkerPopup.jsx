84773209a5e816e8da80dd66a54396efeca6ff47
suesanjp <a.drop.of.my.essence@gmail.com>
2021-09-26 16:24:51 +0900

    fix: MarkerPopup use Array map

--- frontend/src/components/MarkerPopup.jsx
+++ frontend/src/components/MarkerPopup.jsx
@@ -1,37 +1,26 @@
 import { Marker, Popup } from "react-leaflet";
 import mydata from "../mydata.json";
 
-let num = 1;
-const name = mydata[num].name;
-const area = mydata[num].area;
-const genre = mydata[num].genre;
-const address = mydata[num].address;
-const tel = mydata[num].tel;
-const businessHours = mydata[num].business_hours;
-const holiday = mydata[num].holiday;
-const longitude = mydata[num].longitude;
-const latitude = mydata[num].latitude;
-
 const MarkerPopup = () => {
   return (
-    <Marker position={[longitude, latitude]}>
-      <Popup>
-        {name}
-        <br />
-        {area}
-        <br />
-        {genre}
-        <br />
-        {address}
-        <br />
-        {tel}
-        <br />
-        {businessHours}
-        <br />
-        {holiday}
-        <br />
-      </Popup>
-    </Marker>
+    <>
+      {mydata.map((item, index) => (
+        <ul key={index}>
+          <li>
+            <Marker position={[item.longitude, item.latitude]}>
+              <Popup>
+                <h4>{item.name}</h4>
+                <p>{item.genre}</p>
+                <p>{item.address}</p>
+                <p>電話番号: {item.tel}</p>
+                <p>営業時間: {item.business_hours}</p>
+                <p>定休日: {item.holiday}</p>
+              </Popup>
+            </Marker>
+          </li>
+        </ul>
+      ))}
+    </>
   );
 };
 
